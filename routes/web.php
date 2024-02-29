fbadc22a895d6169662fc4c5c3785b2b39c82f79
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-02-29 22:48:45 +0300

    controllers

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ExampleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ExampleController::class, 'homePage']);
+
+Route::get('/about', [ExampleController::class, 'aboutPage']);
