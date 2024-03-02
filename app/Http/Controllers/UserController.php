5132ac0a4c64b3a0d2853a868768a0f3a8f9c4c3
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-03-02 23:53:38 +0300

    validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
@@ -11,9 +12,9 @@ class UserController extends Controller
     public function register(Request $request)
     {
         $incomingFields = $request->validate([
-            'username' => 'required',
-            'email' => 'required',
-            'password' => 'required',
+            'username' => ['required', 'min:3', Rule::unique('users', 'username')],
+            'email' => ['required', 'email', Rule::unique('users', 'email')],
+            'password' => ['required', 'min:6', 'confirmed'],
         ]);
         User::create($incomingFields);
         return "User Added Successfully <br> <a href = '/'>Back to Home</a>";
