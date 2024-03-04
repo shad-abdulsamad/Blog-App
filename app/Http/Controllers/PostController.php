584bc62d69d1a34fc4d3570a844323da5d3254c4
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-03-04 18:06:45 +0300

    markdown

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
@@ -24,11 +25,12 @@ class PostController extends Controller
         $incomingFields['user_id'] = auth()->id();
 
         $newPost = Post::create($incomingFields);
-        return redirect("/post/{$newPost->id}")->with('success', 'Post Created Successfully');
+        return redirect("/posts/{$newPost->id}")->with('success', 'Post Created Successfully');
     }
 
     public function showSinglePost(Post $post)
     {
+        $post['body'] = strip_tags(Str::markdown($post->body), '<p><ul><ol><li><strong><em><h1><h2><h3><h4><h5><h6><br>');
         return view('single-post', ["post" => $post]);
     }
 }
