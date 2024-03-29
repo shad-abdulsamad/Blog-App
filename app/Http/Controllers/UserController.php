544ef3a2d24f2010d4a7cdcdc68b31f030ce5794
Shad Abdulsamad <shad.A.A2002@gmail.com>
2024-03-29 17:47:29 +0300

    listing followers and followings of users

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -21,7 +21,7 @@ class UserController extends Controller
             $currentlyFollowing = Follow::where([['user_id', '=', auth()->user()->id], ['followeduser', '=', $user->id]])->count();
         }
 
-        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count()]);
+        View::share('sharedData', ['currentlyFollowing' => $currentlyFollowing, 'avatar' => $user->avatar, 'username' => $user->username, 'postCount' => $user->posts()->count(), 'followerCount' => $user->followers()->count(), 'followingCount' => $user->following()->count()]);
     }
 
     public function profile(User $user)
@@ -33,12 +33,12 @@ class UserController extends Controller
     public function profileFollowers(User $user)
     {
         $this->getSharedProfileData($user);
-        return view('profile-followers', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-followers', ['followers' => $user->followers()->latest()->get()]);
     }
     public function profileFollowing(User $user)
     {
         $this->getSharedProfileData($user);
-        return view('profile-following', ['posts' => $user->posts()->latest()->get()]);
+        return view('profile-following', ['following' => $user->following()->latest()->get()]);
     }
 
     public function logout()
--- app/Models/User.php
+++ app/Models/User.php
@@ -49,6 +49,15 @@ class User extends Authenticatable
         return $this->hasMany(Post::class, 'user_id');
     }
 
+    public function followers()
+    {
+        return $this->hasMany(Follow::class, 'followeduser');
+    }
+    public function following()
+    {
+        return $this->hasMany(Follow::class, 'user_id');
+    }
+
     protected function avatar(): Attribute
     {
         return Attribute::make(get: function ($value) {
